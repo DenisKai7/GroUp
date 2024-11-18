9c057fe1bf2082a30c92a9a2c21fa7f98958f275
fardanio19 <farhatmoh2000@gmail.com>
2024-11-18 23:45:02 +0700

    capstone

---
+++ app/src/main/java/com/example/projectcapstone/Routes.kt
@@ -0,0 +1,10 @@
+package com.example.projectcapstone
+
+object Routes {
+     val LoginScreen = "LoginScreen"
+     val RegisterScreen = "RegisterScreen"
+     val HomePage = "HomePage"
+     val StatusCheckScreen = "StatusCheckScreen"
+     val ProfileScreen = "ProfileScreen"
+     val EditScreen = "EditScreen"
+}
